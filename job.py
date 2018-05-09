d83b50b8b655cd089bc867acb003b95dfe70fc80
jusgolds <jusgolds@gmail.com>
2018-05-08 19:52:39 -0500

    Create Table and while loop

--- job.py
+++ job.py
@@ -1,28 +1,40 @@
-# load data
-filename = input('Enter the file name: ')
-file = open(filename)
-text = file.read()
-file.close()
-# split into words
-from nltk import word_tokenize
-tokens = word_tokenize(text)
-# convert  to lower case
-tokens = [w.lower() for w in tokens]
-# remove punctuation from each word
-import string
-table = str.maketrans('', '', string.punctuation)
-stripped = [w.translate(table) for w in tokens]
-# remove all tokens that are not alphabetic
-words = [word for word in tokens if word.isalpha()]
-# filter out stop words
-from nltk.corpus import stopwords
-stop_words = set(stopwords.words('english'))
-words = [w for w in words if not w in stop_words]
-# count words
-counts = dict()
-for word in words:
-    if word not in counts:
-        counts[word] = 1
-    else:
-        counts[word] += 1
-print(counts)
+while True:
+    # load data
+    filename = input('Enter the file name: ')
+    file = open(filename)
+    text = file.read()
+    file.close()
+    # split into words
+    from nltk import word_tokenize
+    tokens = word_tokenize(text)
+    # convert  to lower case
+    tokens = [w.lower() for w in tokens]
+    # remove punctuation from each word
+    import string
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+    # remove all tokens that are not alphabetic
+    words = [word for word in tokens if word.isalpha()]
+    # filter out stop words
+    from nltk.corpus import stopwords
+    stop_words = set(stopwords.words('english'))
+    words = [w for w in words if not w in stop_words]
+    # count words
+    counts = dict()
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+    print(counts)
+    # create table in db
+    import sqlite3
+    conn = sqlite3.connect('jobs.sqlite')
+    cur = conn.cursor()
+    cur.execute('DROP TABLE IF EXISTS Keywords')
+    cur.execute('CREATE TABLE Keywords (word TEXT, occurences INTEGER)')
+    conn.close()
+    # continue?
+    end = input("Do you have another file (Y/N): ")
+    if end == "N":
+        break
