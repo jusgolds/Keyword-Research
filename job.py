99e009b0749f0651e497d55390df6b370ee1653e
jusgolds <jusgolds@gmail.com>
2018-05-13 18:33:15 -0500

    Rehaul db entry

--- job.py
+++ job.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.sql.expression import ClauseElement
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 
@@ -15,7 +16,7 @@ class Word(Base):
     __tablename__ = 'word'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    count = Column(Integer, nullable=False)
+    count = Column(Integer, default=0, nullable=False)
 
 now = time.strftime("%Y%m%d-%H%M%S")
 db_path = os.path.join('dbs', 'job-{}.db'.format(now))
@@ -26,42 +27,49 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# run script
-while True:
-    # load data
-    filename = input('Enter the file name: ')
-    try:
-        file = open(os.path.join('descriptions',filename), 'r')
-    except:
-        print('File cannot be opened: ', filename)
-        break
-    text = file.read()
-    file.close()
-    # split into words
-    tokens = word_tokenize(text)
-    # convert  to lower case
-    tokens = [w.lower() for w in tokens]
-    # remove punctuation from each word
-    table = str.maketrans('', '', string.punctuation)
-    stripped = [w.translate(table) for w in tokens]
-    # remove all tokens that are not alphabetic
-    words = [word for word in tokens if word.isalpha()]
-    # filter out stop words
-    stop_words = set(stopwords.words('english'))
-    words = [w for w in words if not w in stop_words]
-    # count words
-    counts = dict()
-    for word in words:
-        if word not in counts:
-            counts[word] = 1
-        else:
-            counts[word] += 1
-    for word in counts:
-        db_word = Word(name=word, count=counts[word])
-        session.add(db_word)
+def get_or_create(session, model, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance
+    else:
+        instance = model(**kwargs)
+        session.add(instance)
         session.commit()
-    print(counts)
-    # continue?
-    end = input("Do you have another file (Y/N): ")
-    if end == "N":
-        break
+        return instance
+
+descriptions_dir = 'descriptions'
+
+directory = os.fsencode(descriptions_dir)
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".txt"):
+        with open('{}/{}'.format(descriptions_dir, filename)) as f:
+            text = f.read()
+        # split into words
+        tokens = word_tokenize(text)
+        # convert  to lower case
+        tokens = [w.lower() for w in tokens]
+        # remove punctuation from each word
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [w.translate(table) for w in tokens]
+        # remove all tokens that are not alphabetic
+        words = [word for word in tokens if word.isalpha()]
+        # filter out stop words
+        stop_words = set(stopwords.words('english'))
+        words = [w for w in words if not w in stop_words]
+        # count words
+        counts = dict()
+        for word in words:
+            if word not in counts:
+                counts[word] = 1
+            else:
+                counts[word] += 1
+        for word in counts:
+            db_word = get_or_create(session, Word, name=word)
+            db_word.count += counts[word]
+            session.add(db_word)
+            session.commit()
+        print("Finished {}".format(filename))
+
+print("Finished all files")
